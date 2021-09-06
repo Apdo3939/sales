90da50c1802a07d439ce23aefdb767b321b7b87b
Apdo3939 <apdo3939@hotmail.com>
2021-09-06 13:01:32 -0300

    Add components navbar and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import Footer from "components/footer";
+import Navbar from "components/navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-primary">Vendas</h1>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container">
+        <p className="text-primary">
+          App desenvolvido por{" "}
+          <a
+            href="https://github.com/apdo3939"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alexandre Pereira{" "}
+          </a>
+        </p>
+        <p className="text-primary">
+          <small>
+            <strong>Dashboard vendas</strong>
+            <br />
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
