8adb0b2b817dfe79ce520700dc84afd413d49723
Apdo3939 <apdo3939@hotmail.com>
2021-09-06 15:27:26 -0300

    Add component BarChart and DonutChart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
+import BarChart from "components/barChart";
 import DataTable from "components/dataTable";
+import DonutChart from "components/donutChart";
 import Footer from "components/footer";
 import Navbar from "components/navbar";
 
 function App() {
   return (
-    <div className="container">
+    <>
       <Navbar />
-      <DataTable />
+      <div className="container">
+        <h1 className="text-primary py-3">Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div>
+          <h3 className="text-center text-primary py-3">Todas as vendas</h3>
+        </div>
+        <DataTable />
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- frontend/src/components/dataTable/index.tsx
+++ frontend/src/components/dataTable/index.tsx
@@ -14,31 +14,38 @@ const DataTable = () => {
         <tbody>
           <tr className="text-primary">
             <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
+            <td>Anakin</td>
+            <td>100</td>
+            <td>43</td>
+            <td>43600.00</td>
           </tr>
           <tr className="text-primary">
             <td>22/04/2021</td>
             <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
+            <td>100</td>
+            <td>67</td>
+            <td>67100.00</td>
           </tr>
           <tr className="text-primary">
             <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
+            <td>Kal-El</td>
+            <td>100</td>
+            <td>67</td>
+            <td>67700.00</td>
           </tr>
           <tr className="text-primary">
             <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
+            <td>Logan</td>
+            <td>100</td>
+            <td>45</td>
+            <td>45600.00</td>
+          </tr>
+          <tr className="text-primary">
+            <td>22/04/2021</td>
+            <td>Padme</td>
+            <td>100</td>
+            <td>71</td>
+            <td>71100.00</td>
           </tr>
         </tbody>
       </table>
