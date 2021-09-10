e040d670e51392697aa48d31c0bc1e78f48d1310
Apdo3939 <apdo3939@hotmail.com>
2021-09-10 14:48:38 -0300

    Add BarChart Integrate with api

--- frontend/src/components/barChart/index.tsx
+++ frontend/src/components/barChart/index.tsx
@@ -1,6 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: [
+    {
+      name: string;
+      data: number[];
+    }
+  ];
+};
 
 const BarChart = () => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
+
   const options = {
     plotOptions: {
       bar: {
@@ -9,24 +38,39 @@ const BarChart = () => {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1],
-      },
-    ],
-  };
+  useEffect(() => {
+    axios
+      .get(BASE_URL + "/sales/success-by-seller")
+      .then((response) => {
+        const data = response.data as SaleSuccess[];
+        const myLabels = data.map((x) => x.seller);
+        const mySeries = data.map((x) =>
+          round((100.0 * x.deals) / x.visited, 1)
+        );
+
+        setChartData({
+          labels: {
+            categories: myLabels,
+          },
+          series: [
+            {
+              name: "% Success",
+              data: mySeries,
+            },
+          ],
+        });
+      })
+      .catch(() => {
+        console.log("Erro: Tente mais tarde!");
+      });
+  }, []);
 
   return (
     <div>
       <Chart
-        options={{ ...options, xaxis: mockData.labels }}
+        options={{ ...options, xaxis: chartData.labels }}
         height={240}
-        series={mockData.series}
+        series={chartData.series}
         type="bar"
       />
     </div>
